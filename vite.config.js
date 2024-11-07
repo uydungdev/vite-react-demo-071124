873d872415ab779eca843f27305e7c122c1086dc
ntd187 <ntdung1@gmail.com>
2024-11-07 22:25:44 +0700

    update base path is same as github repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/vite-react-demo/",
+  base: "/vite-react-demo-071124/",
   plugins: [react()],
 })
